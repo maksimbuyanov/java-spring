53b1d7b53724c380c29535db56cc8aeda1352be0
maksim <maksim.buyanov@gmail.com>
2024-08-25 14:51:01 +0300

    18 реализовал пару контроллеров для теста страниц

--- src/main/java/maks/test/HelloController.java
+++ src/main/java/maks/test/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/helloBro")
     public String hello() {
         System.out.println("Hello World!");
         return "hello";
