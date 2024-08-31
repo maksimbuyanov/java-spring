45d7848ca0bcd98d2c3a4bde375218cdd5b0ac46
maksim <maksim.buyanov@gmail.com>
2024-08-31 19:37:58 +0300

    20 проверил работу с query параметрами

--- src/main/java/maks/test/controllers/FirstController.java
+++ src/main/java/maks/test/controllers/FirstController.java
@@ -1,20 +1,30 @@
 package maks.test.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {// NOTE НЕ падает в ошибку если параметров нет
+        String name = request.getParameter("name");
+        String surname = request.getParameter("secondName");
+
+        System.out.println(name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage() {
+    public String goodbyePage(
+            @RequestParam("name") String name,
+            @RequestParam(value = "secondName", required = false) String surname) { // NOTE Падает в ошибку если параметров нет, иногда нужен required = false
+
+        System.out.println(name + " " + surname);
         return "first/goodbye";
     }
 }
