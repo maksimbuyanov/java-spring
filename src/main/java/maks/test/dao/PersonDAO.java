8af545d3dddc0d5fece7fa6ecfcc39e2bcc8e8d2
maksim <maksim.buyanov@gmail.com>
2024-09-06 18:49:28 +0300

    23 добавил форму создания персоны

--- src/main/java/maks/test/dao/PersonDAO.java
+++ src/main/java/maks/test/dao/PersonDAO.java
@@ -31,4 +31,9 @@ public class PersonDAO {
                 .findFirst()
                 .orElse(null);
     }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
 }
